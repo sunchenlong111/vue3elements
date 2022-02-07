c8960030fe2ef27ae1f5258185000f637e23d609
sunchenlong <chenlong.sun@lingyue-digital.com>
2022-02-07 15:51:14 +0800

    switch基础样式

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-  Switch 
-  </div>
+	<div>
+		<Switch v-model:checked="checked"></Switch>
+	</div>
 </template>
+<script lang="ts">
+import Switch from "../libs/Switch.vue";
+import {ref} from "vue";
+export default {
+	components: {Switch},
+	setup() {
+		const checked = ref(false);
+    return {
+      checked
+    }
+	},
+};
+</script>
