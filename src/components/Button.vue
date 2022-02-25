db088a6c6d17a3159b5987021b76767801e3fe94
sunchenlong <chenlong.sun@lingyue-digital.com>
2022-02-25 16:49:44 +0800

    vue3 button 属性绑定

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-  Button 
-  </div>
+	<div>
+		<Button @click="onClick" @focus="onClick" @mouseover="onClick" size="small">你好</Button>
+	</div>
 </template>
+<script lang="ts">
+import Button from "../libs/Button.vue";
+import {ref} from "vue";
+export default {
+	components: {Button},
+	setup() {
+		const onClick = () => {
+			console.log("hi");
+		};
+
+		return {onClick};
+	},
+};
+</script>
