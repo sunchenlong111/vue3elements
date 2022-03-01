8040f506ad7f8bcd9bf278955e3c40d9495396aa
sunchenlong111 <1094584419@qq.com>
2022-03-01 17:18:20 +0800

    add dialog 组件

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-  DIalog
-  </div>
+	<div>Dialog 示例</div>
+	<h1>示例1</h1>
+	<Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from "../libs/Dialog.vue";
+export default {
+	components: {
+		Dialog,
+	},
+};
+</script>
