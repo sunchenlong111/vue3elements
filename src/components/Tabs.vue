8d87f664edeadd1a9466f0f37a2f63573931272d
sunchenlong111 <1094584419@qq.com>
2022-03-07 17:51:50 +0800

    tabs组件完善

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-  Tabs
-  </div>
+	<h3>tab demo</h3>
+	<Tabs v-model:selected="title">
+		<Tab title="nav1">content 1</Tab>
+		<Tab title="nav2222222">content 222222</Tab>
+	</Tabs>
 </template>
+
+<script lang="ts">
+import Tab from "../libs/Tab.vue";
+import Tabs from "../libs/Tabs.vue";
+import {ref} from "vue";
+export default {
+	components: {
+		Tabs,
+		Tab,
+	},
+	setup() {
+    const title = ref('nav1')
+    return {
+      title
+    }
+  },
+};
+</script>
+
+<style></style>
